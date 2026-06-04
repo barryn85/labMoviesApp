4f2e55d584cc4e0a29a996736ad6ed73f15fa7ee
Barry Nolan <bazatron85@gmail.com>
2026-06-04 10:14:25 +0100

    Movie details page components.

--- moviesApp/src/types/interfaces.ts
+++ moviesApp/src/types/interfaces.ts
@@ -1,3 +1,10 @@
  export interface BaseMovieListProps { 
     movies: BaseMovieProps[];
   }   
+
+    export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }
