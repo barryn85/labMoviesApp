baa78343a51b4bf75317625ed4dbe6da9f5577c8
Barry Nolan <bazatron85@gmail.com>
2026-06-04 10:34:34 +0100

    Movie country added.

--- moviesApp/src/types/interfaces.ts
+++ moviesApp/src/types/interfaces.ts
@@ -8,3 +8,29 @@
       name: string;
     }[];
   }
+
+  export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+}
+
+export interface MoviePageProps {
+  movie: MovieDetailsProps;
+  images: MovieImage[];
+}
+
+export interface MovieDetailsProps extends BaseMovieProps {
+  genres: {
+    id: number;
+    name: string;
+  }[];
+
+  production_countries: {
+    name: string;
+  }[];
+}
