996a0c58de7fa7afcf84384418bffb8e98e80571
Barry Nolan <bazatron85@gmail.com>
2026-06-12 14:54:19 +0100

    Exercise - Show favourites icon on Movie details page.

--- moviesApp/src/components/headerMovie/index.tsx
+++ moviesApp/src/components/headerMovie/index.tsx
@@ -5,34 +5,53 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Avatar from "@mui/material/Avatar";
+import { MovieDetailsProps } from "../../types/interfaces";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+  const favourites = JSON.parse(
+    localStorage.getItem("favourites") || "[]"
+  );
+
+  const isFavourite = favourites.some(
+    (fav: { id: number }) => fav.id === movie.id
+  );
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+      {isFavourite && (
+        <Avatar sx={styles.avatar}>
+          <FavoriteIcon />
+        </Avatar>
+      )}
+
       <Typography variant="h4" component="h3">
-        {movie.title}{"   "}
+        {movie.title}{" "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
-        <span>{`${movie.tagline}`} </span>
+        <span>{movie.tagline}</span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
